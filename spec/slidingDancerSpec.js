0ae06f4b29204edd7e43a602cb826a2a9c82b596
glofishh <gloriaulee@gmail.com>
2019-04-16 15:15:53 -0400

    css stuff

---
+++ spec/slidingDancerSpec.js
@@ -0,0 +1,28 @@
+describe('slidingDancer', function() {
+
+  var slidingDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    slidingDancer = new MakeSlidingDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(slidingDancer.$node).to.be.an.instanceof(jQuery);
+  });
+  
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(slidingDancer, 'step');
+      expect(slidingDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(slidingDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(slidingDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
---
+++ spec/twirlyDancerSpec.js
@@ -0,0 +1,28 @@
+describe('twirlyDancer', function() {
+
+  var twirlyDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    twirlyDancer = new MakeTwirlyDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(twirlyDancer.$node).to.be.an.instanceof(jQuery);
+  });
+  
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(twirlyDancer, 'step');
+      expect(twirlyDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(twirlyDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(twirlyDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
